c9ab2a4a6cd95c87c4b64496d6c7a4c2801feea8
Iremar Pereira <ramerips3@yahoo.com>
2020-04-14 21:55:23 -0300

    Projeto Criado

---
+++ src/main/java/com/rameripscursos/RameripsCursosApplication.java
@@ -0,0 +1,13 @@
+package com.rameripscursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RameripsCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RameripsCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rameripscursos/RameripsCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rameripscursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RameripsCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
