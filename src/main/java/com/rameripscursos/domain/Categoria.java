391f23fd6dac2319ecda468103542f0e3bc5a0e4
Iremar Pereira <ramerips3@yahoo.com>
2020-04-15 13:30:55 -0300

    Testando a primeira classe de domínio - Categoria

---
+++ src/main/java/com/rameripscursos/domain/Categoria.java
@@ -0,0 +1,78 @@
+package com.rameripscursos.domain;
+
+import java.io.Serializable;
+
+//Atalho para a importação das bibliotecas necessárias, click em "CTRL + SHIFT + O"
+//Aqui abaixo temos a criação da classe principal deste layout
+public class Categoria implements Serializable {
+	//O "serialVersionUID = 1L" é gerado para implementar o "Serializable" acima
+	private static final long serialVersionUID = 1L;
+	
+	//Aqui abaixo temos a criação dos atributos e seus tipos da classe "Categoria" 
+	private Integer id;
+	private String nome;
+	
+	//Atalho para implementação de construtores, click em "ALT + SHIFT + S" e escolha opção
+	//Aqui temos a implementação obrigatória de um construtor vazio da classe "Categoria"
+	public Categoria() {		
+	}
+	
+	//Aqui temos a implementação obrigatória de um construtor povoando seu atributos
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	//Aqui abaixo temos todos os "Gets e Sets" desta classe "Categoria"
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//Aqui abaixo gera um código numérico para cada objeto
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	//Aqui abaixo faz a comparação entre dois objetos considerando várias possibilidades
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+}
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/rameripscursos/resources/CategoriaResource.java
+++ src/main/java/com/rameripscursos/resources/CategoriaResource.java
@@ -1,10 +1,15 @@
 package com.rameripscursos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Para o click de teclas de atalho de importações das bibliotecas usa-se -> "CTRL +SHIFT + O"
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rameripscursos.domain.Categoria;
 
 //Com o click "CTRL + SPACE" é exibido um autocomplete de opções
 @RestController
@@ -12,7 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		//Aqui abaixo está sendo criado dois novos objetos da classe "Categoria"
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		//A importação da classe "List" é do tipo "java.util.List"
+		List<Categoria> lista = new ArrayList<>();
+		//Aqui abaixo será adicionado à lista os objeto "cat1 e cat2"
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		//Aqui foi implementado para teste do desenvolvedor
+		//return "REST está funcionando!";
+		
+		//Aqui abaixo será retornado a lista de categorias.
+		return lista;
 	}
 }
